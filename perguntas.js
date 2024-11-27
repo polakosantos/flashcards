78f12f452c32803e35af90143e660a4e638b537a
luis miguel dos santos <106595389+polakosantos@users.noreply.github.com>
2024-11-27 10:47:07 -0300

    correção

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Charadas',
+    'quais são os trễs números, nenhum dos quais é zero , que dão o mesmo resultado, quer sejam somados ou multiplicados',
+    '1+2+3 =6 e 1x2 x3=6'
+)
+
+criaCartao(
+    'Charadas',
+    'Cai de pé e corre deitado',
+    'A chuva'
+)
+
+criaCartao(
+    'Charadas',
+    'feito para andar mas não anda',
+    'A rua'
+)
+
+criaCartao(
+    'Charadas',
+    'Tem escama e coroa mas não é peixe',
+    'Abacaxi'
+)
+
+criaCartao(
+    'Charadas',
+    'Vai e vem com as tripa de fora',
+    'A agulha'
+)
+
+criaCartao(
+    'Charadas',
+    'Nasce, corre e nuca para',
+    'Agua do rio'
+)
